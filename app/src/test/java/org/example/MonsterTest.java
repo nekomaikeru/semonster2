d6130e1c4467444cd8305f5316166222d5e7fd54
dice-2004 <135799601+dice-2004@users.noreply.github.com>
2026-07-14 17:28:18 +0900

    Update MonsterTest.java

--- app/src/test/java/org/example/MonsterTest.java
+++ app/src/test/java/org/example/MonsterTest.java
@@ -10,7 +10,7 @@ public class MonsterTest {
     String name = "スライム";
     int rare = 0;
     Monster monster = new Monster(name, rare);
-    assertEquals("スライム" + ":レア度[" + "0" + "]", monster.toString());
+    assertEquals("スライム:レア度[0]", monster.toString());
   }
 
   @Test
